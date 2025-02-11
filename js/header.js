9f5d28017afe90ca94288302b2ae46ac1445d7a3
Miragecheck <alesha112211@gmail.com>
2025-02-11 10:35:20 +0300

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,176 @@
+var headerText = `<!--Начало заголовка -->
+<div data-rsssl="1" itemtype="https://schema.org/WebPage" itemscope="itemscope" class="home page-template-default page page-id-10 theme-astra woocommerce-js wpt_device_desktop wpt_table_body wpt-body-Product_Table ast-desktop ast-page-builder-template ast-no-sidebar astra-4.0.2 ast-single-post ast-inherit-site-logo-transparent ast-hfb-header elementor-default elementor-kit-12 elementor-page elementor-page-10 e--ua-blink e--ua-chrome e--ua-webkit dialog-body dialog-lightbox-body dialog-container dialog-lightbox-container" data-elementor-device-mode="desktop">
+<div class="hfeed site" id="page">
+			<div data-elementor-type="header" data-elementor-id="14" class="elementor elementor-14 elementor-location-header" data-elementor-post-type="elementor_library">
+			
+				<div class="elementor-element elementor-element-805a5f8 e-flex e-con-boxed e-con e-parent" data-id="805a5f8" data-element_type="container">
+					<div class="e-con-inner">
+						<div class="elementor-element elementor-element-1ddf21d e-con-full elementor-hidden-tablet elementor-hidden-mobile e-flex e-con e-child" data-id="1ddf21d" data-element_type="container">
+							<div class="elementor-element elementor-element-b50a994 elementor-search-form--skin-classic elementor-search-form--button-type-icon elementor-search-form--icon-search elementor-widget elementor-widget-search-form" data-id="b50a994" data-element_type="widget" data-settings="{&quot;skin&quot;:&quot;classic&quot;}" data-widget_type="search-form.default">
+							<div class="elementor-widget-container"></div></div></div>
+
+<!--Поиск-->
+				<div class="elementor-element elementor-element-1ddf21d e-con-full elementor-hidden-tablet elementor-hidden-mobile e-flex e-con e-child" data-id="1ddf21d" data-element_type="container">
+					<div class="elementor-element elementor-element-b50a994 elementor-search-form--skin-classic elementor-search-form--button-type-icon elementor-search-form--icon-search elementor-widget elementor-widget-search-form" data-id="b50a994" data-element_type="widget" data-settings="{&quot;skin&quot;:&quot;classic&quot;}" data-widget_type="search-form.default">
+					<div class="elementor-widget-container">
+				<link rel="stylesheet" href="./css/widget-theme-elements.min.css">		
+				
+				<search role="search">
+				<form class="elementor-search-form" action="" method="get">
+													<div class="elementor-search-form__container">
+						<label class="elementor-screen-only" for="elementor-search-form-b50a994">Search</label>
+	
+						
+						<input id="elementor-search-form-b50a994" placeholder="Поиск..." class="elementor-search-form__input" type="search" name="s" value="">
+						
+												<button class="elementor-search-form__submit" type="submit" aria-label="Search">
+																<div class="e-font-icon-svg-container"><svg class="fa fa-search e-font-icon-svg e-fas-search" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+		 <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></div>								<span class="elementor-screen-only">Search</span>
+														</button>
+						
+										</div>
+				</form>
+			</search>
+			
+					</div>
+					</div>
+					</div>
+<!--Поиск-->
+<!--Заказать звонок-->
+<div class="elementor-element elementor-element-f96a601 e-con-full e-flex e-con e-child" data-id="f96a601" data-element_type="container">
+	<div class="elementor-element elementor-element-fa5e397 elementor-widget__width-auto elementor-widget elementor-widget-button" data-id="fa5e397" 		data-element_type="widget" data-widget_type="button.default">
+		<div class="elementor-widget-container">
+			<div class="elementor-button-wrapper">
+				<button class="elementor-button elementor-button-link elementor-size-xs" href="#win-call" id="btn-call" >
+					<span class="elementor-button-content-wrapper">
+						<span class="elementor-button-text">ЗАКАЗАТЬ ЗВОНОК</span>
+					</span>
+				</button>						
+			</div>
+		</div>
+	</div>
+</div>	
+<!--Заказать звонок-->
+					</div>
+				</div>
+			
+				
+<!--Лого-->
+		<div class="elementor-element elementor-element-e52911b e-flex e-con-boxed e-con e-parent" data-id="e52911b" data-element_type="container">
+					<div class="e-con-inner">
+				<div class="elementor-element elementor-element-bc92403 e-con-full e-flex e-con e-child" data-id="bc92403" data-element_type="container">
+					<div class="elementor-element elementor-element-336b415 elementor-widget elementor-widget-image" data-id="336b415" data-element_type="widget" data-widget_type="image.default">
+						<div class="elementor-widget-container"><a href=""></a>	
+							<img   src="./image/KingFloor1.jpg" class="attachment-full size-full wp-image-15" alt=""/>	
+						</div>
+					</div>
+				</div>
+
+				<div class="elementor-element elementor-element-bc92403 e-con-full e-flex e-con e-child" data-id="bc92403" data-element_type="container">
+					<div class="elementor-element elementor-element-336b415 elementor-widget elementor-widget-image" data-id="336b415" data-element_type="widget" data-widget_type="image.default">
+						<div class="elementor-widget-container"><a href=""></a>
+							<img  src="./image/PrideTekBlue1.jpg" class="attachment-full size-full wp-image-15" alt=""/>	
+						</div>
+					</div>
+				</div>
+<!--Лого-->
+		<div class="elementor-element elementor-element-9860cd8 e-con-full e-flex e-con e-child" data-id="9860cd8" data-element_type="container">
+				<div class="elementor-element elementor-element-eb2d369 elementor-widget elementor-widget-heading" data-id="eb2d369" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<span class="elementor-heading-title elementor-size-default"><a href="tel:+78002505775" target="_blank">8-800-250-57-75</a></span>		</div>
+				</div>
+				<div class="elementor-element elementor-element-a3ece54 elementor-widget elementor-widget-heading" data-id="a3ece54" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<div class="elementor-heading-title elementor-size-default">ЗВОНКИ ИЗ РОССИИ БЕСПЛАТНЫЕ<br>
+КРУГЛОСУТОЧНО</div>		</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-d6e4f08 e-con-full e-flex e-con e-child" data-id="d6e4f08" data-element_type="container">
+				<div class="elementor-element elementor-element-cddb513 elementor-widget elementor-widget-heading" data-id="cddb513" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<span class="elementor-heading-title elementor-size-default"><a href="tel:+79105600514" target="_blank">8 (910) 560-05-14</a></span><br>		
+					</div>
+				</div>
+				<div class="elementor-element elementor-element-336bb0f elementor-widget elementor-widget-heading" data-id="336bb0f" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<div class="elementor-heading-title elementor-size-default">C 9:00 ДО 18:00</div>		</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-9293041 e-con-full elementor-hidden-mobile e-flex e-con e-child" data-id="9293041" data-element_type="container">
+				<div class="elementor-element elementor-element-18fa7b1 elementor-widget elementor-widget-heading" data-id="18fa7b1" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<span class="elementor-heading-title elementor-size-default"><a href="mailto:pr@pridetech.ru" target="_blank">pr@pridetech.ru</a></span>		</div>
+				</div>
+				</div>
+		<div class="elementor-element elementor-element-99711f5 e-con-full e-flex e-con e-child" data-id="99711f5" data-element_type="container">
+				<div class="elementor-element elementor-element-56218fe elementor-nav-menu__align-center elementor-nav-menu--dropdown-none elementor-widget elementor-widget-nav-menu" data-id="56218fe" data-element_type="widget" data-settings="{&quot;layout&quot;:&quot;horizontal&quot;,  &quot;submenu_icon&quot;:{&quot;value&quot;:&quot;&lt;svg class=\&quot;e-font-icon-svg e-fas-caret-down\&quot; viewBox=\&quot;0 0 320 512\&quot; xmlns=\&quot;http:\/\/www.w3.org\/2000\/svg\&quot;&gt;&lt;path d=\&quot;M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z\&quot;&gt;&lt;\/path&gt;&lt;\/svg&gt;&quot;,  &quot;library&quot;:&quot;fa-solid&quot;}}" data-widget_type="nav-menu.default">
+				<div class="elementor-widget-container">
+			<link rel="stylesheet" href="./css/widget-nav-menu.min.css">			
+			<nav class="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-horizontal e--pointer-none">
+				<ul id="menu-1-56218fe" class="elementor-nav-menu" data-smartmenus-id="17271876359178032"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-128"><a href="https://cmtpr.ru/#" class="elementor-item elementor-item-anchor menu-link">Элемент меню</a></li>
+<li class="menu-item wpmenucart wpmenucartli wpmenucart-display-standard menu-item menu-item-type-custom menu-item-object-custom menu-item-128 wp-block-navigation-item wp-block-navigation-link empty">
+	
+	<script type="text/javascript" src="./css/busket.js"></script>
+	<a class="wpmenucart-contents empty-wpmenucart-visible wp-block-navigation-item__content">
+		<a class="cartcontents" style="cursor: pointer" onclick="busket()"> <img src="image/icon-buscet.png">Корзина</a></a>
+
+	
+	
+	
+</li></ul>			</nav>
+						
+				</div>
+				</div>
+				</div>
+					</div>
+				</div>
+		<div class="elementor-element elementor-element-838fde4 e-flex e-con-boxed e-con e-parent"  data-id="838fde4" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+					<div class="e-con-inner">
+				<div class="elementor-element elementor-element-7de4698 elementor-nav-menu__align-center elementor-nav-menu--stretch elementor-nav-menu--dropdown-tablet elementor-nav-menu__text-align-aside elementor-nav-menu--toggle elementor-nav-menu--burger elementor-widget elementor-widget-nav-menu" data-id="7de4698" data-element_type="widget" data-settings="{&quot;full_width&quot;:&quot;stretch&quot;,  &quot;layout&quot;:&quot;horizontal&quot;,  &quot;submenu_icon&quot;:{&quot;value&quot;:&quot;&lt;svg class=\&quot;e-font-icon-svg e-fas-caret-down\&quot; viewBox=\&quot;0 0 320 512\&quot; xmlns=\&quot;http:\/\/www.w3.org\/2000\/svg\&quot;&gt;&lt;path d=\&quot;M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z\&quot;&gt;&lt;\/path&gt;&lt;\/svg&gt;&quot;,  &quot;library&quot;:&quot;fa-solid&quot;},  &quot;toggle&quot;:&quot;burger&quot;}" data-widget_type="nav-menu.default">
+				<div class="elementor-widget-container">
+						<nav class="elementor-nav-menu--main elementor-nav-menu__container elementor-nav-menu--layout-horizontal e--pointer-none">
+				
+							<ul id="menu-1-7de4698" class="elementor-nav-menu" data-smartmenus-id="17271876359183728">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-10 current_page_item menu-item-33">
+<a href="index.html" aria-current="page" class="elementor-item elementor-item-active menu-link">Главная</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-100"><a href="catalog.html" class="elementor-item menu-link has-submenu">Продукция/каталог<span role="presentation" class="dropdown-menu-toggle"></span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-563"><a href="" class="elementor-item menu-link has-submenu">Область применения</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-592"><a href="" class="elementor-item menu-link has-submenu">Фото объектов</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="" class="elementor-item menu-link has-submenu">Видео</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-588"><a href="" class="elementor-item menu-link has-submenu">Цена, условия поставки</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-565"><a href="" class="elementor-item menu-link has-submenu">Контакты</a></li>
+</ul>			</nav>
+					
+				</div>
+				</div>
+				
+					</div>
+				</div>
+				</div>
+			<div id="content" class="site-content">
+
+		<div class="ast-container">
+	<div id="primary" class="content-area primary">
+
+		
+					<main id="main" class="site-main">
+				<article class="post-10 page type-page status-publish ast-article-single" id="post-10" itemtype="https://schema.org/CreativeWork" itemscope="itemscope">
+	</div>
+	<!-- Конец заголовка -->`;
+
+   // var footerText = `Подвал, сюда код подвала писать`;
+
+function setHeader() {
+  var header =  document.createElement("div");
+  header.innerHTML = headerText ;
+  document.body.insertAdjacentElement('afterbegin', header );
+}
+
+/*function setFooter() {
+  var footer =  document.createElement("footer");
+  footer.innerHTML = footerText ;
+  document.body.insertAdjacentElement('afterend', footer );
+}
+
+setFooter();// JavaScript Document*/
+setHeader();

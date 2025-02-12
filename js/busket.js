7be4485a9d2f12451a1f7318df9d8a8d71d7309d
Miragecheck <alesha112211@gmail.com>
2025-02-12 12:00:51 +0300

    Add files via upload

---
+++ js/busket.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var orders = 0;
+//var bttn = document.getElementById("busketOrders");
+
+function busket(){
+    var text =  "В связи с волонтильностью цен, он-лайн оплата временно недоступна, Ваш заказ будет направлен менеджеру для уточнения цен и наличия на складе";
+    alert(text);
+    //document.getElementById('busketOrders').textContent = text;
+}

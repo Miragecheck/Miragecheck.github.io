568fb240ada01e57d78c25b48d350de95c11046e
Miragecheck <alesha112211@gmail.com>
2025-02-12 12:00:08 +0300

    Delete js directory

--- js/spisok.js
+++
@@ -1,5 +0,0 @@
-const hamburger = document.querySelector('.hamburger')
-const ulMobLinks = document.querySelector('.mob-links')
-
-hamburger.addEventListener('click', ()=> {  ulMobLinks.classList.toggle('active');
-})
